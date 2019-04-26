83cf3930fa60d102f6ee4baced8a9d1c00c4cbeb
Boris Fazio <boris.fazio@outlook.com>
2019-04-25 22:14:08 -0500

    setup now includes an edit to brms:::reloo.loo to properly handle leaving out data points in stanvars

--- code/setup.R
+++ code/setup.R
@@ -1,6 +1,18 @@
 library(tidyverse)
 library(brms)
 
+# Modifying package functions
+  # Tell reloo to also leave out the trials passed via stanvars
+tmp.fun <- brms:::reloo.loo
+body(tmp.fun)[[12]][[4]][7:9] <- body(tmp.fun)[[12]][[4]][6:8]
+body(tmp.fun)[[12]][[4]][[6]] <- substitute(
+  if(!is.null(fit_j$stanvars$trials)){
+    fit_j$stanvars$trials$sdata <- fit_j$stanvars$trials$sdata[-omitted]
+    fit_j$stanvars$trials$scode <- "  int trials[639];"
+  })
+assignInNamespace("reloo.loo", value = tmp.fun, ns = "brms")
+
+
 # Convenience functions
   # Paste operator
   # Saves space when writing folder paths
@@ -12,6 +24,7 @@ save_fit <- function(fitname, fitfun, formula_, ...) {
   if(file.exists("out/fits/" %p0% fitname %p0% ".rds"))stop("File already exists")
   saveRDS(object = fitfun(formula_, ...), file = "out/fits/" %p0% fitname %p0% ".rds")
 }
+
 
   # Pre-defined models
 source("code/models.R")
